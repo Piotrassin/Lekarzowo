7733d597cee68064700859dbf8a6a2aaa357c44f
Pitorek <piotrassin@gmail.com>
2020-05-11 21:23:48 +0200

    Kolejne widoki rozpoczęte

--- Lekarzowo/Models/View_IllnessMedList.cs
+++ Lekarzowo/Models/View_IllnessMedList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,30 @@ namespace Lekarzowo.Models
 {
     public class View_IllnessMedList
     {
+        [Column("Patient_Id")]
+        public int PatientId { get; set; }
+        [Column("Person_Name")]
+        public String PatientName { get; set; }
+
+        [Column("Person_Lastname")]
+        public String PatientLastname { get; set; }
+
+        [Column("Reservation_Id")]
+        public int ReservationId { get; set; }
+
+        [Column("Reservation_StartTime")]
+        public DateTime ReservationStartTime { get; set; }
+
+        [Column("Illness_Name")]
+        public String IllnessName { get; set; }
+
+        [Column("IllnessHistory_CureDate")]
+        public DateTime? IllnessHistoryCureDate { get; set; }
+
+        [Column("OldIllnessHistory_Date")]
+        public DateTime OldIllnessHistoryDate { get; set; }
+
+        [Column("OldIllnessHistory_CureDate")]
+        public DateTime? OldIllnessHistoryCureDate { get; set; }
     }
 }
--- Lekarzowo/Models/View_PatientIllnesses.cs
+++ Lekarzowo/Models/View_PatientIllnesses.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,27 @@ namespace Lekarzowo.Models
 {
     public class View_PatientIllnesses
     {
+        [Column("Patient_Id")]
+        public int PatientId { get; set; }
+
+        [Column("Person_Name")]
+        public String PatientName { get; set; }
+
+        [Column("Person_Lastname")]
+        public String PatientLastname { get; set; }
+
+        [Column("Illness_Name")]
+        public String IllnessName { get; set; }
+
+        [Column("IllnessHistory_Id")]
+        public int IllnessHistoryId { get; set; }
+
+        [Column("IllnessHistory_CureDate")]
+        public DateTime? IllnessHistoryCureDate { get; set; }
+
+        [Column("OldIllnessHistory_Id")]
+        public int OldIllnessHistoryId { get; set; }
+
+
     }
 }
