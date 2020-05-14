05c8f0d27413b2a6fd77552a4e3f9fea4a1d38fe
Pitorek <piotrassin@gmail.com>
2020-05-14 17:00:49 +0200

    VisitController i DoctorController wygenerowane przy pomocy  EF

---
+++ Lekarzowo/Controllers/DoctorsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lekarzowo.Models;
+
+namespace Lekarzowo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+        private readonly ModelContext _context;
+
+        public DoctorsController(ModelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Doctors
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Doctor>>> GetDoctor()
+        {
+            return await _context.Doctor.ToListAsync();
+        }
+
+        // GET: api/Doctors/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Doctor>> GetDoctor(decimal id)
+        {
+            var doctor = await _context.Doctor.FindAsync(id);
+
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return doctor;
+        }
+
+        // PUT: api/Doctors/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDoctor(decimal id, Doctor doctor)
+        {
+            if (id != doctor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(doctor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DoctorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Doctors
+        [HttpPost]
+        public async Task<ActionResult<Doctor>> PostDoctor(Doctor doctor)
+        {
+            _context.Doctor.Add(doctor);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (DoctorExists(doctor.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetDoctor", new { id = doctor.Id }, doctor);
+        }
+
+        // DELETE: api/Doctors/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Doctor>> DeleteDoctor(decimal id)
+        {
+            var doctor = await _context.Doctor.FindAsync(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Doctor.Remove(doctor);
+            await _context.SaveChangesAsync();
+
+            return doctor;
+        }
+
+        private bool DoctorExists(decimal id)
+        {
+            return _context.Doctor.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Lekarzowo/Controllers/VisitsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lekarzowo.Models;
+
+namespace Lekarzowo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitsController : ControllerBase
+    {
+        private readonly ModelContext _context;
+
+        public VisitsController(ModelContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Visits
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Visit>>> GetVisit()
+        {
+            return await _context.Visit.ToListAsync();
+        }
+
+        // GET: api/Visits/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Visit>> GetVisit(decimal id)
+        {
+            var visit = await _context.Visit.FindAsync(id);
+
+            if (visit == null)
+            {
+                return NotFound();
+            }
+
+            return visit;
+        }
+
+        // PUT: api/Visits/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVisit(decimal id, Visit visit)
+        {
+            if (id != visit.ReservationId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(visit).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VisitExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Visits
+        [HttpPost]
+        public async Task<ActionResult<Visit>> PostVisit(Visit visit)
+        {
+            _context.Visit.Add(visit);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (VisitExists(visit.ReservationId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetVisit", new { id = visit.ReservationId }, visit);
+        }
+
+        // DELETE: api/Visits/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Visit>> DeleteVisit(decimal id)
+        {
+            var visit = await _context.Visit.FindAsync(id);
+            if (visit == null)
+            {
+                return NotFound();
+            }
+
+            _context.Visit.Remove(visit);
+            await _context.SaveChangesAsync();
+
+            return visit;
+        }
+
+        private bool VisitExists(decimal id)
+        {
+            return _context.Visit.Any(e => e.ReservationId == id);
+        }
+    }
+}
