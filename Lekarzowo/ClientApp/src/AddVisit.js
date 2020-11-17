5667bd5da8eb3ebe54f0f365e44a099a040dd995
grzegorzwaszkiewicz <grzegorzwaszkiewicz1997@gmail.com>
2020-11-17 23:55:36 +0100

    Update AddVisit

--- Lekarzowo/ClientApp/src/AddVisit.js
+++ Lekarzowo/ClientApp/src/AddVisit.js
@@ -6,8 +6,34 @@ import UserService from './services/UserService.js';
 import Calendar from 'react-calendar'
 import oneSign from './images/1Sign.svg'
 import twoSign from './images/2Sign.svg'
+import threeSign from './images/3Sign.svg'
 import './Main.css';
-import 'react-calendar/dist/Calendar.css';
+import './css/helpers/Calendar.css';
+import {
+  withStyles,
+  makeStyles,
+  createMuiTheme,
+} from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const CssTextField = withStyles({
+  root: {
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'grey !important',
+      },
+      '& label': {
+      color: 'green !important',
+      },
+      '&:hover fieldset': {
+        borderColor: 'yellow',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'green',
+      },
+    },
+  },
+})(TextField);
 
 class AddVisit extends React.Component {
 constructor(props){
@@ -106,15 +132,38 @@ render() {
             </div>
           </div>
           <div className = 'calendar-container flex-column'>
-          <div className = 'container-header'>
-            <img src = {twoSign} className = "addSign" style = {{width: 25, marginRight: "10px"}} />
-            <a style = {{color: "#1859A6"}}>Wybierz Datę</a>
-
-          </div>
-          <br/><br/>
-          <Calendar />
-          </div>
-
+            <div className = 'container-header'>
+              <img src = {twoSign} className = "addSign" style = {{width: 25, marginRight: "10px"}} />
+              <a style = {{color: "#1859A6"}}>Wybierz Datę</a>
+              </div>
+              <br/><br/>
+              <Calendar
+              className = 'calendar-custom'
+              selectRange
+              />
+              <br/>
+              <div className = 'container-header'>
+                <img src = {threeSign} className = "addSign" style = {{width: 25, marginRight: "10px"}} />
+                <a style = {{color: "#1859A6"}}>Wybierz Godziny</a>
+                </div><br/>
+              <div className="flex-column">
+                <CssTextField variant = "outlined"
+                label="Godzina Od"
+                type="time"
+                defaultValue="07:30"
+                size="small"/>
+                <br/>
+                <CssTextField variant = "outlined"
+                label="Godzina Do"
+                type="time"
+                defaultValue="22:00"
+                size="small"/>
+              </div>
+              <br/>
+              <div className = "btn-container">
+                <button className = "btn-primary">Szukaj</button>
+              </div>
+            </div>
         </div>
       </div>
     </div>
