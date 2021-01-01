f48c7ceb594c65c881a124349970f1129ea6ed54
Pitorek <piotrassin@gmail.com>
2021-01-01 22:46:28 +0100

    Końcówki zwracające nadchodzące i przeszłe rezerwacje wizyt dla lekarza i pacjenta, pokazują teraz jako "nadchodzące", również te, które powinny się były zacząć półgodziny temu.

--- Lekarzowo/DataAccessLayer/Repositories/ReservationsRepository.cs
+++ Lekarzowo/DataAccessLayer/Repositories/ReservationsRepository.cs
@@ -101,8 +101,8 @@ namespace Lekarzowo.DataAccessLayer.Repositories
                 .Where(x => x.Starttime.Date >= start.Value.Date && x.Endtime.Date <= end.Value.Date);
 
             query = showRecent
-                ? query.Where(x => x.Visit != null && x.Canceled == false)
-                : query.Where(x => x.Visit == null);
+                ? query.Where(x => x.Visit != null && x.Canceled == false).Where(x => x.Starttime < DateTime.Now)
+                : query.Where(x => x.Visit == null).Where(x => x.Starttime.AddMinutes(30) >= DateTime.Now);
 
             var anonymousQuery = query.Select(s => new
             {
@@ -119,15 +119,16 @@ namespace Lekarzowo.DataAccessLayer.Repositories
             });
 
             IOrderedQueryable<object> orderedQuery = showRecent
-                ? anonymousQuery.Where(x => x.reservationStartTime < DateTime.Now).OrderByDescending(x => x.reservationStartTime)
-                : anonymousQuery.Where(x => x.reservationStartTime >= DateTime.Now).OrderBy(x => x.reservationStartTime);
+                ? anonymousQuery.OrderByDescending(x => x.reservationStartTime)
+                : anonymousQuery.OrderBy(x => x.reservationStartTime);
 
             var trimmedQuery = PaginationService<object>.SplitAndLimitQueryable(skip, limit, orderedQuery);
 
             return await trimmedQuery.ToListAsync();
         }
 
-        public async Task<IEnumerable<object>> RecentOrUpcomingByPatientId(decimal patientId, bool showUpcomingInstead, bool hideCanceledReservations, decimal? doctorId, DateTime? from, DateTime? to, int? limit, int? skip)
+        public async Task<IEnumerable<object>> RecentOrUpcomingByPatientId(decimal patientId, bool showUpcomingInstead, 
+            bool hideCanceledReservations, decimal? doctorId, DateTime? from, DateTime? to, int? limit, int? skip)
         {
             var reservationTypeQuery = _context.Reservation
                 .Where(x => x.PatientId == patientId)
@@ -141,8 +142,13 @@ namespace Lekarzowo.DataAccessLayer.Repositories
             }
 
             reservationTypeQuery = showUpcomingInstead
-                ? reservationTypeQuery.Where(x => x.Visit == null)
-                : reservationTypeQuery.Where(x => x.Visit != null);
+                ? reservationTypeQuery
+                    .Where(x => x.Visit == null)
+                    .Where(x => x.Starttime.AddMinutes(30) >= DateTime.Now)
+                : reservationTypeQuery
+                    .Where(x => x.Visit != null)
+                    .Where(x => x.Visit.OnGoing == false)
+                    .Where(x => x.Starttime < DateTime.Now);
 
             var anonymousTypeQuery = reservationTypeQuery.Select(x => new
             {
@@ -165,8 +171,8 @@ namespace Lekarzowo.DataAccessLayer.Repositories
             });
 
             IOrderedQueryable<object> orderedQuery = showUpcomingInstead
-                ? anonymousTypeQuery.Where(x => x.reservationStartTime >= DateTime.Now).OrderBy(x => x.reservationStartTime)
-                : anonymousTypeQuery.Where(x => x.reservationStartTime < DateTime.Now).OrderByDescending(x => x.reservationStartTime);
+                ? anonymousTypeQuery.OrderBy(x => x.reservationStartTime)
+                : anonymousTypeQuery.OrderByDescending(x => x.reservationStartTime);
 
             var trimmedQuery = PaginationService<object>.SplitAndLimitQueryable(skip, limit, orderedQuery);
 
