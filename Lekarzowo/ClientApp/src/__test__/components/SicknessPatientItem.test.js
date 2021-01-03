f2b2145eabc3482c6157b414920f1aee2f5d3118
grzegorzwaszkiewicz <grzegorzwaszkiewicz1997@gmail.com>
2021-01-03 22:10:08 +0100

    Dodany test dla SicknessPatientItem

---
+++ Lekarzowo/ClientApp/src/__test__/components/SicknessPatientItem.test.js
@@ -0,0 +1,35 @@
+import SicknessPatientItem from '../../components/SicknessPatientItem.js';
+import {shallow, mount, debug} from 'enzyme';
+import {render, fireEvent, wait, cleanup} from '@testing-library/react';
+import React from 'react';
+
+const sickness = {
+  illnessName: "Ból głowy",
+  description: "Zdiagnozowano ból w tylnej części głowy"
+};
+
+
+
+it('renders', () => {
+  shallow(<SicknessPatientItem sickness = {sickness} />);
+});
+
+it('accepts props', () => {
+  const wrapper = mount(<SicknessPatientItem sickness = {sickness} />);
+  expect(wrapper.props().sickness.illnessName).toEqual(sickness.illnessName);
+  expect(wrapper.props().sickness.description).toEqual(sickness.description);
+});
+
+describe("Component functionality", () => {
+
+  it('displays details', () => {
+    const {debug} = render(<SicknessPatientItem sickness = {sickness} />);
+    debug();
+    const wrapper = mount(<SicknessPatientItem sickness = {sickness} />);
+    var value = wrapper.find("b").at(0).text();
+    expect(value).toEqual("Ból głowy");
+    value = wrapper.find("a").at(0).text();
+    expect(value).toEqual("Zdiagnozowano ból w tylnej części głowy");
+  });
+
+});
