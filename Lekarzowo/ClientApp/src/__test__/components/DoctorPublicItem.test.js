42c95f533546990fab14ae2e21f77bff09acadc7
grzegorzwaszkiewicz <grzegorzwaszkiewicz1997@gmail.com>
2021-01-03 13:47:39 +0100

    Dodany test dla doctorPublicItem

---
+++ Lekarzowo/ClientApp/src/__test__/components/DoctorPublicItem.test.js
@@ -0,0 +1,45 @@
+import DoctorPublicItem from '../../components/DoctorPublicItem.js';
+import {shallow, mount, debug} from 'enzyme';
+import {render, fireEvent} from '@testing-library/react';
+import React from 'react';
+
+const user = {
+  name: "Andrzej Andrzejewski",
+  id: 1
+};
+
+const clickCallback = (id) => {
+  return id
+}
+
+
+it('renders', () => {
+  shallow(<DoctorPublicItem />);
+});
+
+it('accepts props', () => {
+  const wrapper = mount(<DoctorPublicItem name = {user.name} id = {user.id} clickCallback = {clickCallback}/>);
+  expect(wrapper.props().name).toEqual(user.name);
+  expect(wrapper.props().id).toEqual(user.id);
+});
+
+describe("Component functionality", () => {
+
+  it('displays name', () => {
+    const wrapper = mount(<DoctorPublicItem name = {user.name} id = {user.id} />);
+    const value = wrapper.find("a").text();
+    expect(value).toEqual("Andrzej Andrzejewski");
+  });
+
+  it('firesButton',  () => {
+    const mockedCallBack = jest.fn((id)  => {
+      return id
+    });
+    const { getByRole } = render(<DoctorPublicItem name = {user.name} id = {user.id} clickCallback = {mockedCallBack}/>);
+    var btn = getByRole('button', {name: ""});
+    fireEvent.click(btn);
+    expect(mockedCallBack.mock.results[0].value).toEqual(1);
+  });
+
+
+});
