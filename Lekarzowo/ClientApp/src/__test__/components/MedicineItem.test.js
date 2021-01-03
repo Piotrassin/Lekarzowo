5f4614eef6915261acb87d568cd8f62aa9ddccbc
grzegorzwaszkiewicz <grzegorzwaszkiewicz1997@gmail.com>
2021-01-03 14:04:54 +0100

    Dodany test dla MedicineItem

---
+++ Lekarzowo/ClientApp/src/__test__/components/MedicineItem.test.js
@@ -0,0 +1,30 @@
+import MedicineItem from '../../components/MedicineItem.js';
+import {shallow, mount, debug} from 'enzyme';
+import {render, fireEvent} from '@testing-library/react';
+import React from 'react';
+
+const medicine = {
+  medicineName: "Apap 10mg",
+  medicineDosage: "Stosować 1 dziennie - wieczorem"
+};
+
+it('renders', () => {
+  shallow(<MedicineItem medicine = {medicine} />);
+});
+
+it('accepts props', () => {
+  const wrapper = mount(<MedicineItem medicine = {medicine} />);
+  expect(wrapper.props().medicine.medicineName).toEqual(medicine.medicineName);
+  expect(wrapper.props().medicine.medicineDosage).toEqual(medicine.medicineDosage);
+});
+
+describe("Component functionality", () => {
+
+  it('displays medicine details', () => {
+    const wrapper = mount(<MedicineItem medicine = {medicine}/>);
+    var value = wrapper.find("a").at(0).text();
+    expect(value).toEqual("Apap 10mg");
+    value = wrapper.find("a").at(1).text();
+    expect(value).toEqual("Stosować 1 dziennie - wieczorem");
+  });
+});
