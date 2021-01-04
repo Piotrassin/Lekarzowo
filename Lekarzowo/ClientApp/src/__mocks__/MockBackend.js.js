949e8558338df1bc83a94ca9a0b963f4c6bfdf86
grzegorzwaszkiewicz <grzegorzwaszkiewicz1997@gmail.com>
2021-01-04 23:08:08 +0100

    Dodany test dla AdminAddSickness

--- Lekarzowo/ClientApp/src/__mocks__/MockBackend.js.js
+++ Lekarzowo/ClientApp/src/__mocks__/MockBackend.js.js
@@ -123,6 +123,13 @@ class MockBackend{
          json: () => Promise.resolve({}),
        }
      }
+     case '/Illnesses': {
+       return {
+         ok: true,
+         status: 200,
+         json: () => Promise.resolve({}),
+       }
+     }
      default: {
        throw new Error(`Unhandled request: ${splittedUrl}`)
      }
