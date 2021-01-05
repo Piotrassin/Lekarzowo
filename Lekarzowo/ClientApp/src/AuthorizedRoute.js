717703c7ced0ca829027ce1b901720d2f56b684e
grzegorzwaszkiewicz <grzegorzwaszkiewicz1997@gmail.com>
2021-01-05 13:55:07 +0100

    Usuniete komentarze

--- Lekarzowo/ClientApp/src/AuthorizedRoute.js
+++ Lekarzowo/ClientApp/src/AuthorizedRoute.js
@@ -7,13 +7,9 @@ export const AuthorizedRoute = ({ component: Component, roles, ...rest }) => (
     <Route {...rest} render={props => {
         const currentUser = AuthService.getUser();
         if (currentUser && currentUser.status == 401) {
-            // not logged in so redirect to login page with the return url
             return <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
         }
-
-        // check if route is restricted by role
         if (roles && roles.indexOf(currentUser.currentRole) === -1) {
-            // role not authorised so redirect to home page
             switch(currentUser.currentRole){
               case 'patient':
                 return <Redirect to={{ pathname: '/'}} />
@@ -29,9 +25,7 @@ export const AuthorizedRoute = ({ component: Component, roles, ...rest }) => (
         }
         if(props.render){
           return props.render;
-
         }
-        // authorised so return component
         return <Component {...props} />
     }} />
 )
