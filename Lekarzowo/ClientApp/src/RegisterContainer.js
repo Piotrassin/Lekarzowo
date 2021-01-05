c2fc7318d49140497fd4e78584916099de59e152
grzegorzwaszkiewicz <grzegorzwaszkiewicz1997@gmail.com>
2021-01-05 17:14:54 +0100

    RegisterContainer cleanup

--- Lekarzowo/ClientApp/src/RegisterContainer.js
+++ Lekarzowo/ClientApp/src/RegisterContainer.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import Dashboard from './Dashboard'
-import Login from './Login'
-import Visits from './Visits'
 import AuthService from './authentication/AuthService.js'
 import Registration from './Registration.js'
-import {
-  Route,
-  NavLink,
-  HashRouter,
-  Redirect
-} from "react-router-dom";
 
 class RegisterContainer extends React.Component {
   constructor(props){
     super(props);
     if(AuthService.getLoggedUser() != null){
-      console.log("Already authenticated");
-      this.props.history.push('/dashboard');
+      this.props.history.push('/');
     }
   }
 
