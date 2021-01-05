22dae345bfde7c12c408c4ae4a01e4c1d6e8fbad
grzegorzwaszkiewicz <grzegorzwaszkiewicz1997@gmail.com>
2021-01-05 16:54:38 +0100

    ProfileMedicine cleanup

--- Lekarzowo/ClientApp/src/ProfileMedicine.js
+++ Lekarzowo/ClientApp/src/ProfileMedicine.js
@@ -23,21 +23,17 @@ componentDidMount() {
   }, () => {
     UserService.getUserMedicineHistory()
     .then(response => {
-      console.log('response: ');
-      console.log(response[0]);
       this.setState({
-      medicines: response
-      });
-      this.setState({
-        loading: false
+      medicines: response,
+      loading: false
       });
     })
     .catch(err => {
         try{
-  this.state.snackbarRef.current.openSnackBar(err.message, 'red-snackbar');
-}catch(erorr){
-  console.log('Missed Reference');
-};
+          this.state.snackbarRef.current.openSnackBar(err.message, 'red-snackbar');
+        }catch(erorr){
+          console.log('Missed Reference');
+        };
     });
   });
 
@@ -49,40 +45,34 @@ render() {
       <a className = 'subheader-content-profile'>Leki Pacjenta</a>
       <a className = 'subheading-content-profile'>Aktualne</a>
       <div className = 'flex-column width-100'>
-      <div className = 'sickness-item'>
-        <div className = 'sickness-item-part part-1'>
+        <div className = 'sickness-item'>
+          <div className = 'sickness-item-part part-1'>
+          </div>
+          <div className = 'sickness-item-part part-2'>
+            <a className = 'table-header'>Lek</a>
+          </div>
+          <div className = 'sickness-item-part part-3'>
+            <a className = 'table-header'>Dawka</a>
+          </div>
+          <div className = 'sickness-item-part part-4'>
+            <a className = 'table-header'>Okres przyjmowania</a>
+          </div>
+          <div className = 'sickness-item-part part-5'>
+          </div>
         </div>
-        <div className = 'sickness-item-part part-2'>
-          <a className = 'table-header'>Lek</a>
-        </div>
-        <div className = 'sickness-item-part part-3'>
-          <a className = 'table-header'>Dawka</a>
-        </div>
-        <div className = 'sickness-item-part part-4'>
-          <a className = 'table-header'>Okres przyjmowania</a>
-        </div>
-        <div className = 'sickness-item-part part-5'>
-
-        </div>
+        {this.state.medicines && this.state.medicines.map((medicine, index ) => (
+        <MedicineItem
+        medicine={medicine}
+        />
+        ))}
       </div>
-{this.state.medicines && this.state.medicines.map((medicine, index ) => (
-    <MedicineItem
-    medicine={medicine}
-    />
-  ))}
-
-      </div>
-
       <br/>
       <div className='progress-holder'>
-        <Fade
-          in={this.state.loading}
-          unmountOnExit
-        >
+        <Fade in={this.state.loading} unmountOnExit>
           <LinearProgress />
-          </Fade>
-        </div>
-        <Snackbar ref = {this.state.snackbarRef} />
+        </Fade>
+      </div>
+      <Snackbar ref = {this.state.snackbarRef} />
     </div>
   );
 }
