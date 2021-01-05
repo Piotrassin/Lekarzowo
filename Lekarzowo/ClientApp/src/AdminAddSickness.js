a0ef80b96d74182a5caaa5cc250098e0ba060ace
grzegorzwaszkiewicz <grzegorzwaszkiewicz1997@gmail.com>
2021-01-05 13:31:37 +0100

    AdminAddSickness cleanup

--- Lekarzowo/ClientApp/src/AdminAddSickness.js
+++ Lekarzowo/ClientApp/src/AdminAddSickness.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import SicknessItem from './components/SicknessItem.js';
 import AdminService from './services/AdminService.js';
-import Fade from '@material-ui/core/Fade';
-import TextField from '@material-ui/core/TextField';
-import LinearProgress from '@material-ui/core/LinearProgress';
 import Snackbar from './helpers/Snackbar.js';
-import Autocomplete from './components/Autocomplete.js';
 import Validation from './helpers/Validation.js';
+import Autocomplete from './components/Autocomplete.js';
+import TextField from '@material-ui/core/TextField';
 
 class AdminAddSickness extends React.Component {
 constructor(props){
@@ -31,15 +28,12 @@ handleClickAddIllness(event){
   this.setState ({
     errors: Validation.validateUniversalBlank(this.state.illnessName, "Pole Nazwa choroby")
   }, () => {
-    console.log(this.state.errors);
     if(Object.keys(this.state.errors).length > 0){
       var message = Validation.handleValidationOutcome(this.state.errors);
       this.snackbarRef.current.openSnackBar( message ,'red-snackbar');
-
     }else {
       AdminService.postIllness(this.state.illnessName)
       .then(response => {
-        console.log(response);
         this.setState({
           illnessName: ""
         });
@@ -57,10 +51,6 @@ handleClickAddIllness(event){
 
 }
 
-componentDidMount() {
-
-}
-
 render() {
   return(
     <div className = 'admin-content-holder flex-column' >
@@ -75,14 +65,13 @@ render() {
             type = 'text'
             size="small" fullWidth />
           </div>
-
           <br/><br/>
           <div>
-          <a className = 'button-green' onClick = {this.handleClickAddIllness}>Dodaj</a>
+            <a className = 'button-green' onClick = {this.handleClickAddIllness}>Dodaj</a>
           </div>
-          </form>
-          </div>
-        <Snackbar ref = {this.snackbarRef} classes = 'green-snackbar' />
+        </form>
+      </div>
+      <Snackbar ref = {this.snackbarRef} classes = 'green-snackbar' />
     </div>
   );
 }
