4731921424657b010b89aefb87306a9e41331f3a
Pitorek <piotrassin@gmail.com>
2021-01-06 17:56:13 +0100

    Dodałem końcówkę PATCH umożliwiającą edycję opisu wizyty bez zmiany jej pozostałych elementów. Domyślna metoda PUT w klasie Visit, nie pozwala teraz edytować pola OnGoing. Jest to możliwe jedynie przez końcówkę visit/changestatus

---
+++ Lekarzowo/DataAccessLayer/DTO/VisitDescriptionOnlyDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Lekarzowo.DataAccessLayer.DTO
+{
+    public class VisitDescriptionOnlyDTO : IEntity
+    {
+        public decimal Id { get; set; }
+        public string Description { get; set; }
+    }
+}
